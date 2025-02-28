a11ba47f2f9532b8a1c36391a0b1a4c5a7b29b94
Trisha Goodwin <trishabromero@gmail.com>
2025-02-27 21:13:55 -0600

    prompted user, created array of string responses

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//Propt user to enter a string 
+const userFlavors = prompt(
+    "Type froyo flavors separated by a comma",
+    "vanilla,chocolate,strawberry"
+  );
 
+//Turn string into array  
+const flavorArray = userFlavors.split(",");
+
+console.log(flavorArray);
+
+//..to be continued...
